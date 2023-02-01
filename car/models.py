4ffb21dcce84cecd914584335f6239cbed841c46
Tunahan Tatli <tunahantatl@gmail.com>
2023-02-01 18:25:01 +0300

    edit-car_app-models-add-reservation

--- car/models.py
+++ car/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.validators import MinValueValidator
+from users.serializers import User
 
 # Create your models here.
 class Car(models.Model):
@@ -11,8 +13,21 @@ class Car(models.Model):
     model = models.CharField(max_length=255)
     year = models.SmallIntegerField()
     gear = models.CharField(max_length=1, choices=Gear)
-    rent_per_day = models.IntegerField()
+    rent_per_day = models.DecimalField(
+        max_digits=6, # for price 
+        decimal_places=2,
+        validators = [MinValueValidator(1)]
+    )
     availability = models.BooleanField(default=True)
 
     def __str__(self):
         return f'{self.plate_number}-{self.brand}-{self.model}-{self.gear}-{self.rent_per_day}'
+
+class Reservation(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name= 'customers')
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name= 'cars')
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+    def __str__(self):
+        return f'Customer:{self.customer}-Reserved: {self.car}- Start Date: {self.start_date}- End Date: {self.end_date}'
